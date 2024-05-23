f1adf290d65631b4cc9a78d13d8fa7ac2bcc7858
Ahmed Latif <105422581+Ahmed-latif@users.noreply.github.com>
2024-05-23 18:31:40 +0500

    Update api_key.dart

--- lib/components/api_key.dart
+++ lib/components/api_key.dart
@@ -1,3 +1,3 @@
-String openSourceKey = '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';
+String openSourceKey = '';
 String baseUrl = 'https://api.openai.com/v1';
 String baseUrl2 = 'https://api.openai.com/v1/images';
